57199873228999939495012ca50c8f582ad19ffe
Rabia Williams <rabiawilliams@gmail.com>
2019-11-13 23:46:13 +1000

    Added React components from library

--- src/webparts/demoWebpart/components/DemoWebpart.tsx
+++ src/webparts/demoWebpart/components/DemoWebpart.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import styles from './DemoWebpart.module.scss';
 import { IDemoWebpartProps } from './IDemoWebpartProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import {MyName} from 'bootcamp-library-component';
 export default class DemoWebpart extends React.Component<IDemoWebpartProps, {}> {
   
 
@@ -16,10 +17,7 @@ export default class DemoWebpart extends React.Component<IDemoWebpartProps, {}> 
               <p className={ styles.subTitle }>The name of my library is </p>
               <p className={ styles.description }>{escape(this.props.libName)}</p>
               <p className={ styles.subTitle }>The name of the developer is </p>
-              <p className={ styles.description }>{escape(this.props.devName)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
+              <MyName username={"Rabia Joanne Williams"}/>
             </div>
           </div>
         </div>
